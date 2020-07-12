3be79da0d66dab0dc10b8ae99f16bf645535d74a
Eric Lavesson <eric.lavesson@gmail.com>
2020-07-13 00:46:25 +0200

    Add include dir for apps with dependencies

--- src/cmake/app.js
+++ src/cmake/app.js
@@ -6,6 +6,14 @@ const linkLibs = (name, options) => {
     return `target_link_libraries(${name} ${deps.join(' ')})`
 };
 
+const includeDirs = (name, options) => {
+    const deps = options.applications[name].dependencies;
+
+    if (!deps || deps.length === 0) return '';
+
+    return 'include_directories(${CMAKE_SOURCE_DIR}/src/libs)';
+};
+
 module.exports = ({name, options, files}) => {
     return `add_executable(
     ${name}
@@ -13,5 +21,6 @@ module.exports = ({name, options, files}) => {
 )
 
 ${linkLibs(name, options)}
+${includeDirs(name, options)}
 `;
 };
--- test/cmakeAppGenTests.js
+++ test/cmakeAppGenTests.js
@@ -17,4 +17,40 @@ describe('cmake app gen tests', () => {
         const app = sut.app("pelle");
         expect(app).to.contain('target_link_libraries(pelle kalle nisse)');
     });
+
+    it('should add expected lib include dir', async () => {
+        const sut = cmakeGen({
+            applications: {
+                'pelle': {
+                    dependencies: [
+                        'kalle'
+                    ]
+                }
+            }
+        }, {
+            libs: 'libs',
+        }, {
+            libEntryPoint: { impl: 'lib.cpp', header: 'lib.h' }
+        });
+
+        const lib = sut.app("pelle");
+        expect(lib).to.contain('include_directories(${CMAKE_SOURCE_DIR}/src/libs)');
+    });
+
+    it('does not add lib include path if no dependencies', async () => {
+        const sut = cmakeGen({
+            applications: {
+                'pelle': {
+                    dependencies: []
+                }
+            }
+        }, {
+            libs: 'libs',
+        }, {
+            libEntryPoint: { impl: 'lib.cpp', header: 'lib.h' }
+        });
+
+        const lib = sut.app("pelle");
+        expect(lib).to.not.contain('include_directories(${CMAKE_SOURCE_DIR}/src/libs)');
+    });
 });
